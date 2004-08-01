ec2c73782bac01431d0ec3729c9e051e2e9847a4
oddsock <oddsock@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2004-08-01 03:00:19 +0000

    fixed subversion checkout instructions...
    git-svn-id: http://svn.xiph.org/websites/icecast.org@7441 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- svn.php
+++ svn.php
@@ -24,93 +24,7 @@ A  icecast/debian/control
 ....
 Checked out revision 6618.  # Note your revision may be different
 </pre>
-<h3>3. Checkout the m4 module</h3>
-<pre>
-$ cd $HOME/xiphrepository/icecast
-$ svn co http://svn.xiph.org/icecast/trunk/m4/ m4
-A  m4/acx_pthread.m4
-A  m4/shout.m4
-A  m4/xiph_compiler.m4
-A  m4/theora.m4
-A  m4/vorbis.m4
-A  m4/xiph_path_python.m4
-A  m4/xiph_xml2.m4
-A  m4/ac_config_libconfig_in.m4
-A  m4/xiph_types.m4
-A  m4/ogg.m4
-A  m4/xiph_curl.m4
-A  m4/ogg2.m4
-A  m4/xiph_net.m4
-Checked out revision 6618.
-</pre>
-<h3>4. Checkout all dependent icecast modules</h3>
-<pre>
-$ cd $HOME/xiphrepository/icecast/src
-$ svn co http://svn.xiph.org/icecast/trunk/avl/ avl
-A  avl/test.c
-A  avl/avl.c
-A  avl/TODO
-A  avl/avl.dsp
-A  avl/avl.h
-A  avl/COPYING
-A  avl/Makefile.am
-A  avl/.cvsignore
-A  avl/README
-A  avl/BUILDING
-Checked out revision 6618.
-$ svn co http://svn.xiph.org/icecast/trunk/thread/ thread
-A  thread/TODO
-A  thread/COPYING
-A  thread/thread.c
-A  thread/Makefile.am
-A  thread/thread.h
-A  thread/BUILDING
-A  thread/README
-A  thread/.cvsignore
-Checked out revision 6618.
-$ svn co http://svn.xiph.org/icecast/trunk/httpp/ httpp
-A  httpp/test.c
-A  httpp/httpp.c
-A  httpp/TODO
-A  httpp/httpp.h
-A  httpp/COPYING
-A  httpp/Makefile.am
-A  httpp/README
-A  httpp/BUILDING
-A  httpp/.cvsignore
-Checked out revision 6618.
-$ svn co http://svn.xiph.org/icecast/trunk/net/ net
-A  net/sock.c
-A  net/test_resolver.c
-A  net/resolver.c
-A  net/TODO
-A  net/sock.h
-A  net/resolver.h
-A  net/COPYING
-A  net/Makefile.am
-A  net/README
-A  net/BUILDING
-A  net/.cvsignore
-Checked out revision 6618.
-$ svn co http://svn.xiph.org/icecast/trunk/log/ log
-A  log/test.c
-A  log/log.c
-A  log/log.h
-A  log/Makefile.am
-A  log/.cvsignore
-Checked out revision 6618.
-$ svn co http://svn.xiph.org/icecast/trunk/timing/ timing
-A  timing/TODO
-A  timing/timing.c
-A  timing/COPYING
-A  timing/Makefile.am
-A  timing/.cvsignore
-A  timing/README
-A  timing/BUILDING
-A  timing/timing.h
-Checked out revision 6618.
-</pre>
-<h3>5. Run autogen.sh in the main icecast root</h3>
+<h3>3. Run autogen.sh in the main icecast root</h3>
 Note: You must have libtool, autoconf, and automake to perform this step.
 <pre>
 $ cd $HOME/xiphrepository/icecast
@@ -130,7 +44,7 @@ config.status: creating config.h
 config.status: executing depfiles commands
 $
 </pre>
-<h3>6. Verfiy everything by performing a make</h3>
+<h3>4. Verfiy everything by performing a make</h3>
 <pre>
 $ cd $HOME/xiphrepository/icecast
 $ make
