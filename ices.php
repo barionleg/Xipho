deed7505dfa5ccce7276a1946fded23060964744
brendan <brendan@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2004-08-28 19:11:41 +0000

    Release ices 0.4
    git-svn-id: http://svn.xiph.org/websites/icecast.org@7656 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- ices.php
+++ ices.php
@@ -7,7 +7,7 @@
 <div class="newscontent">
 <h3>Documentation</h3>
 <br></br>
-<p>Ices comes in two flavors, the 0.3 flavor which supports sending an MP3 stream to an icecast server, and the 2.x flavor which supports sending an Ogg Vorbis stream to an icecast server.</p>
+<p>Ices comes in two flavors, the 0.x flavor which supports sending an MP3 stream to an icecast server, and the 2.x flavor which supports sending an Ogg Vorbis stream to an icecast server.</p>
 <p>Documenation for ices2 can be found <a href="docs/ices/">here</a></p>
 </div>
 <div class="roundbottom">
@@ -26,7 +26,7 @@
 <table border=1 cellpadding=5 cellspacing=0 width=90% bordercolor="#000000">
 <tr><td><b>Utility</b></td><td><b>Description</b></td><td><b>Download Link</b></td></tr>
 <tr><td>ices2 (version 2.0)</td><td>source client for broadcasting in Ogg Vorbis format to an icecast2 server</td><td><a href="http://svn.xiph.org/releases/ices/ices-2.0.0.tar.gz">ices-2.0.0.tar.gz</a></td></tr>
-<tr><td>ices0 (version 0.3)</td><td>source client for broadcasting in MP3 format to an icecast2 server</td><td><a href="http://svn.xiph.org/releases/ices/ices-0.3.tar.gz">ices-0.3.tar.gz</a></td></tr>
+<tr><td>ices0 (version 0.4)</td><td>source client for broadcasting in MP3 format to an icecast2 server</td><td><a href="http://svn.xiph.org/releases/ices/ices-0.4.tar.gz">ices-0.4.tar.gz</a></td></tr>
 </table>
 </div>
 <div class="roundbottom">
