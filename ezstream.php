00a47f907d87f76aad8348f73ead9830bffa0ccf
oddsock <oddsock@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2005-01-05 01:12:42 +0000

    0.2.0 release of ezstream changes.
    git-svn-id: http://svn.xiph.org/websites/icecast.org@8614 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- ezstream.php
+++ ezstream.php
@@ -8,10 +8,10 @@
 <h3>Download</h3>
 <br></br>
 <p>
-Version 0.1.2 is available at <a href="http://downloads.xiph.org/releases/ezstream/ezstream-0.1.2.tar.gz">ezstream-0.1.2.tar.gz</a> - Version 0.1.2 supports handling of a SIGHUP message which will cause ezstream to reread the currently processing playlist.  This only works on unix platforms and so I'm not going to release a 0.1.2 binary for win32.
+Version 0.2.0 is available at <a href="http://downloads.xiph.org/releases/ezstream/ezstream-0.2.0.tar.gz">ezstream-0.2.0.tar.gz</a>
 </p>
 <p>
-A windows binary is available as well at <a href="http://downloads.xiph.org/releases/ezstream/ezstream_win32_0.1.1_setup.exe">ezstream_win32_0.1.1_setup.exe</a>
+A windows binary is available as well at <a href="http://downloads.xiph.org/releases/ezstream/ezstream_win32_0.2.0_setup.exe">ezstream_win32_0.2.0_setup.exe</a>
 </p>
 <br></br>
 <h3>What is it ?</h3>
@@ -91,6 +91,105 @@ file to suit your needs..The following is an example config file :
 <p>
 <b>SVRINFOPUBLIC </b>- Indicates wether to list this stream in a public YP.
 </p>
+<h3>Re-encoding</h3>
+<br></br>
+<p>
+ezstream now supports reencoding.  This means that your output stream need not
+be the same bitrate/samplerate or even format as your input files.
+</p>
+<p>    
+Reencoding is supported via the use of external programs.  When you enable reencoding
+you need to make sure of a few things :
+</p>
+<p>
+1. You define a "decoder" for each type of input file.<br>
+2. You define a "encoder" for each possible type of output stream.<br>
+</p>
+
+<p>
+So if you had a mixture of mp3 and vorbis files in your playlist, you will need to make
+sure that a decoder is provided for each type.  Ezstream will take the output of the
+decoder and send it directly to the specific encoder.  All output of the decoder should
+be written to stdout and should be in raw form.  Most decoder support this mode.  Encoders
+should all be configured also with raw input and should read it from stdin.
+</p>
+
+<p>
+The following decoder/encoders can be used : 
+</p>
+
+<p>
+For MP3 :<br>
+decoder : madplay<br>
+encoder : lame<br>
+</p>
+
+<p>
+For Vorbis :<br>
+decoder : oggdec<br>
+encoder : oggenc<br>
+</p>
+<p>
+For FLAC :<br>
+decoder : FLAC<br>
+encoder : not yet supported by libshout, and thus not by ezstream.<br>
+</p>
+
+<p>
+Additional decoders and encoders may be used, as long as they follow the rules defined above.
+</p>
+<p>
+The following config file section defines the reencoding parameters :
+</p>
+
+<p>
+<pre>
+&lt;reencode&gt;
+        &lt;enable&gt;1&lt;/enable&gt;
+        &lt;encdec&gt;
+        &lt;!-- Support for FLAC decoding (input files) --&gt;
+                &lt;format&gt;FLAC&lt;/format&gt; &lt;!-- format = output stream format --&gt;
+                &lt;match&gt;.flac&lt;/match&gt;  &lt;!-- match = input file format --&gt;
+                &lt;decode&gt;flac -s -d --force-raw-format --sign=signed 
+                              --endian=little @T@ -o -&lt;/decode&gt;
+                &lt;encode&gt;Not supported Yet&lt;/encode&gt;
+        &lt;/encdec&gt;
+        &lt;encdec&gt;
+        &lt;!-- Support for MP3 decoding via madplay, and encoding via LAME --&gt;
+                &lt;format&gt;MP3&lt;/format&gt;
+                &lt;match&gt;.mp3&lt;/match&gt;
+                &lt;decode&gt;madplay -o raw:- @T@ 2&gt;/dev/null&lt;/decode&gt;
+                &lt;encode&gt;lame -r -x -b 56 -s 44.1 --resample 22.05 -a - - 
+                              2&gt;/dev/null&lt;/encode&gt;
+        &lt;/encdec&gt;
+        &lt;encdec&gt;
+        &lt;!-- Support for Vorbis decoding via oggdec, and encoding via oggenc --&gt;
+                &lt;format&gt;VORBIS&lt;/format&gt;
+                &lt;match&gt;.ogg&lt;/match&gt;
+                &lt;decode&gt;oggdec --raw=1 @T@ -o - 2&gt;/dev/null&lt;/decode&gt;
+                &lt;encode&gt;oggenc -Q -r -q 0 --resample=44100 --downmix 
+                              -t "@M@" -c STREAMER=ezstream -&lt;/encode&gt;
+        &lt;/encdec&gt;
+&lt;/reencode&gt;
+</pre>
+</p>
+<p>
+Note that the following keywords can be used :
+</p>
+
+<p>
+@T@ = The fully qualified name of the track being played in the playlist<br>
+@M@ = The metadata for the current track<br>
+</p>
+
+<p>
+All encoding options (bitrate/samplerate/channels/quality) are set as command line options of
+each of the encoders.  Each encoder has slightly different options that control these values.
+The examples here can be used as a guide, but please make sure you check the manual for each
+encoder for the correct encoding options.  In all cases, the encoder should be configured to
+read RAW audio data from stdin.
+</p>
+
 
 </div>
 <div class="roundbottom">
