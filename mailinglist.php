19a15fce5115c4a35703855775f433ed1ca705f1
msmith <msmith@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2005-03-09 03:55:14 +0000

    icecast website had stuff on the mailing lists page talking about a forum, and some list<->forum gateway. I couldn't find any evidence of these existing, so removed this bit.
    git-svn-id: http://svn.xiph.org/websites/icecast.org@9048 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- mailinglist.php
+++ mailinglist.php
@@ -6,10 +6,9 @@
 </div>
 <div class="newscontent">
 <h3>Mailing List</h3>
-<p>In additon to the web-based forum, we have 2 mailing lists devoted to icecast.  The first ([icecast]) is appropriate for general icecast usage questions.  The second ([icecast-dev]) is reserved for any developer related questions.</p>
-<p>We have set up a mailing list <--> forum gateway which will (hopefully) replicate respective postings.  This means, that if you post to the forum, it will be automatically forwarded to the appropriate mailing list, and vice-versa.  This is a new feature for us, so please bear with it.. :)</p>
+<p>We have 2 mailing lists devoted to icecast.  The first ([icecast]) is appropriate for general icecast usage questions.  The second ([icecast-dev]) is reserved for any developer related questions.</p>
 
-<p>To sign up for each mailing list, simple send an email to <font color="yellow"><b>icecast-request@xiph.org</b></font> or <font color="yellow"><b>icecast-dev-request@xiph.org</b></font> and include the text "subscribe" in the message.  Then simply follow the directions from there.  More details as well as access to the web-accessible archives of the mailing lists can be found at <a href="http://lists.xiph.org/mailman/listinfo/icecast">http://lists.xiph.org/mailman/listinfo/icecast</a> and <a href="http://www.xiph.org/icecast/list.html">http://www.xiph.org/icecast/list.html</a></p>
+<p>To sign up for each mailing list, simple send an email to <font color="yellow"><b>icecast-request@xiph.org</b></font> or <font color="yellow"><b>icecast-dev-request@xiph.org</b></font> and include the text "subscribe" in the message.  Then simply follow the directions from there.  More details as well as access to the web-accessible archives of the mailing lists can be found at <a href="http://lists.xiph.org/mailman/listinfo/icecast">http://lists.xiph.org/mailman/listinfo/icecast</a> (for the icecast list) and <a href="http://lists.xiph.org/mailman/listinfo/icecast-dev">http://lists.xiph.org/mailman/listinfo/icecast-dev</a> (for the icecast-dev list)</p>
 </div>
 <div class="roundbottom">
 <img src="/images/corner_bottomleft.jpg" class="corner" style="display: none" />
