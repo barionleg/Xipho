93612c810fd58f475e9d3494aa54c17f6c6742d3
giles <giles@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2006-10-17 18:08:41 +0000

    A user complained about not being able to figure out what icecast was if one didn't already know. Link to the faq from the front page.
    git-svn-id: http://svn.xiph.org/websites/icecast.org@11924 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <? include "common/header.php"; ?>
+<p><strong>icecast</strong> is free <a href="/faq.php">server software</a> for streaming multimedia.</p>
 <? include "news.php"; ?>
 <? include "common/footer.php"; ?>
