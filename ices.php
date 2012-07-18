8ebe49a492c3e70b9dfed87707225ca1697a2f8c
dm8tbr <dm8tbr@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2012-07-18 15:10:58 +0000

    icecast.org website change to the Ices section. - Remove ices0 from text, add deprecated tag. close #1887 - Fix link to ices2 documentation.
    git-svn-id: http://svn.xiph.org/websites/icecast.org@18466 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- ices.php
+++ ices.php
@@ -7,8 +7,9 @@
 <div class="newscontent">
 <h3>Documentation</h3>
 <br></br>
-<p>Ices comes in two flavors, the 0.x flavor which supports sending an MP3 stream to an icecast server, and the 2.x flavor which supports sending an Ogg Vorbis stream to an icecast server.</p>
-<p>Documentation for ices2 can be found <a href="docs/ices-2.0.0/">here</a></p>
+<p>Ices 2.x supports sending an Ogg Vorbis stream to an Icecast server.</p>
+<p>This can be live audio (e.g. from a sound card) or Ogg Vorbis files from a play list.</p>
+<p>Documentation for ices2 can be found <a href="docs/ices-2.0.1/">here</a></p>
 </div>
 <div class="roundbottom">
 <img src="/images/corner_bottomleft.jpg" class="corner" style="display: none" />
@@ -32,7 +33,7 @@
 <a href="http://downloads.us.xiph.org/releases/ices/ices-2.0.1.tar.gz">ices-2.0.1.tar.gz</a>
 </td>
 </tr>
-<tr><td>ices0 (version 0.4)</td><td>source client for broadcasting in MP3 format to an icecast2 server</td><td><a href="http://downloads.us.xiph.org/releases/ices/ices-0.4.tar.gz">ices-0.4.tar.gz</a></td></tr>
+<tr><td>ices0 (deprecated)</td><td>source client for broadcasting in MP3 format to an icecast2 server</td><td><a href="http://downloads.us.xiph.org/releases/ices/ices-0.4.tar.gz">ices-0.4.tar.gz</a></td></tr>
 </table>
 </div>
 <div class="roundbottom">
