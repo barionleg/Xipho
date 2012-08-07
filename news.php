0aa41af18c0a8f0924cd688f619a12a113f02b58
ph3-der-loewe <ph3-der-loewe@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2012-08-07 12:31:20 +0000

    convert ticket IDs to links
    git-svn-id: http://svn.xiph.org/websites/icecast.org@18529 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- news.php
+++ news.php
@@ -22,14 +22,14 @@ to pick up.</p>
   <br />
  <li>Bugfixes -
   <ul>
-   <li> Updated Documentation(Closes: #1238)
-   <li> Fixed double shout_close() (Closes: #720)
+   <li> Updated Documentation(Closes: <a href="https://trac.xiph.org/ticket/1238">#1238</a>)
+   <li> Fixed double shout_close() (Closes: <a href="https://trac.xiph.org/ticket/720">#720</a>)
    <li> Reduced error to warning in case of duplicate serial number.
-   <li> Fixed handling of <reconnectattempts> when set to zero (Closes: #735)
-   <li> Did some code cleanup and hardening (Closes: #1795)
-   <li> Enabled compiler warnings (Closes: #1796)
-   <li> Added <retry-initial> (Closes: #994)
-   <li> Updated handling of empty strings in config file (Closes: #1875)
+   <li> Fixed handling of <reconnectattempts> when set to zero (Closes: <a href="https://trac.xiph.org/ticket/735">#735</a>)
+   <li> Did some code cleanup and hardening (Closes: <a href="https://trac.xiph.org/ticket/1795">#1795</a>)
+   <li> Enabled compiler warnings (Closes: <a href="https://trac.xiph.org/ticket/1796">#1796</a>)
+   <li> Added <retry-initial> (Closes: <a href="https://trac.xiph.org/ticket/994">#994</a>)
+   <li> Updated handling of empty strings in config file (Closes: <a href="https://trac.xiph.org/ticket/1875">#1875</a>)
    <li> Updated build system
   </ul>
   <br />
